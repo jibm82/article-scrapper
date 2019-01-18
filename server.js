7efdc9140543c764fc97eea31b3242c46d3636e5
Jorge Baez <jibm_82@yahoo.com.mx>
2019-01-17 22:00:05 -0600

    Implemented initial views

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const PORT = process.env.PORT || 3000;
 const app = express();
+const exphbs = require("express-handlebars");
 const Scrapper = require("./tools/Scrapper");
 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 app.get("/", (req, res) => {
-  res.send("Sanity check");
+  Scrapper.perform().then(articles => {
+    res.render("index", { articles });
+  });
 });
 
 app.get("/scrape", (req, res) => {
